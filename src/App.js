6dbfe565530b8602ecc6cc8be82d50f4258a361a
wkdus0608 <seongmin020608@gmail.com>
2024-08-17 13:27:02 +0900

    라우터 기능 추가하기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-    .then(responce => responce.json())
-    .then(json => setMovies(json.data.movies));
-    setLoading(false);
-  }, []);
-  console.log(movies); 
-  return (
-  <div>
-    {loading ? <h1>Loading...</h1> : (<div>{movies.map(movie =>
-    <div key={movie.id}>
-      <h2>{movie.title}</h2>
-      <img src={movie.medium_cover_image} />
-      <p>{movie.summary}</p>
-      <ul>
-        {movie.genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
-      <hr />
-    </div>
-      )}</div>)}
-  </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path="/movie"><Detail /></Route>
+      <Route path="/"><Home /></Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,5 @@
+function Detail() {
+    return <h1>It's Detail</h1>;
+}
+
+export default Detail;
